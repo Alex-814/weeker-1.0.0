ccc591da8a721c18d2e507a7bab50d9ac39fcbd7
Alex-814 <mr.bebebe85@gmail.com>
2026-06-19 00:28:39 +0300

    Add files via upload

---
+++ weeker.py
@@ -0,0 +1,11 @@
+import telebot
+import os
+
+TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot('TOKEN')
+
+@bot.message_handler(commands = ['start'])
+def start(message):
+    bot.send_message(message.chat.id, 'ботяра')
+
+bot.polling(non_stop=True)
