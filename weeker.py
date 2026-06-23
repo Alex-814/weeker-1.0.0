3556f777ae0cdcb2ca6f0809b1014f42106a52ee
Alex-814 <mr.bebebe85@gmail.com>
2026-06-24 00:28:09 +0300

    Update weeker.py

--- weeker.py
+++ weeker.py
@@ -2,7 +2,7 @@ import telebot
 import os
 
 TOKEN = os.environ.get('BOT_TOKEN')
-bot = telebot.TeleBot('TOKEN')
+bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands = ['start'])
 def start(message):
